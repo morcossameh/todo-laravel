122f9dfca3e86d150039620f735c30104b299abe
Morcos Sameh <morcossameh@gmail.com>
2023-03-03 15:08:37 +0200

    add Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import Todo from './components/Todo';
+
+const app = createApp({})
+
+app.component('todo', Todo);
+
+app.mount('#app');
